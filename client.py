f2dba2d034d76179d68e7816f29531f91f011eec
qh <WDqiuhao@163.com>
2017-09-12 17:06:10 +0800

    Add files via upload

---
+++ client.py
@@ -0,0 +1,24 @@
+#! /usr/bin/env python3
+
+import socket
+import readline #如果不import这个模块，input中文backspace之后的显示会不正确（不知道为啥）
+#这个模块的原来目的就是input输入自动补全和记录历时输入
+
+endflag = 1;
+
+s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+#host = socket.gethostname()
+#s.bind(('124.16.71.203', 9998))
+host = '124.16.71.203'
+port = 9999
+s.connect((host, port))
+while endflag:
+	cmd = input('Please input msg:')
+	if cmd == 'end!' or cmd == 'end_all!':
+		s.send(cmd.encode())
+		s.close()
+		endflag = 0
+	else:
+		s.send(cmd.encode())
+		data = s.recv(1024)
+		print(data.decode())
