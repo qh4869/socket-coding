7cd4c1dbc1fc18ff40159edcab0b2f63cf32deed
qh <WDqiuhao@163.com>
2018-02-04 21:09:05 +0800

    Update client.py

--- client.py
+++ client.py
@@ -12,16 +12,13 @@ s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
 host = '124.16.71.203'
 port = 9999
 s.connect((host, port))
-try:
-    while endflag:
-            cmd = input('Please input msg:')
-            if cmd == 'end!' or cmd == 'end_all!':
-                    s.send(cmd.encode())
-                    s.close()
-                    endflag = 0
-            else:
-                    s.send(cmd.encode())
-                    data = s.recv(1024)
-                    print(data.decode())
-except ConnectionRefusedError:
-    print('Fail to Connect!')
+while endflag:
+	cmd = input('Please input msg:')
+	if cmd == 'end!' or cmd == 'end_all!':
+		s.send(cmd.encode())
+		s.close()
+		endflag = 0
+	else:
+		s.send(cmd.encode())
+		data = s.recv(1024)
+print(data.decode())
